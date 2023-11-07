70cdc0a130fce5097f90bd2d10d5317081ada8a4
unknown <rizalpangandaran9@gmail.com>
2023-11-07 10:06:35 +0700

    additional-monthlyreport

--- Modules/MonthlyReport/Http/Controllers/MonthlyReportController.php
+++ Modules/MonthlyReport/Http/Controllers/MonthlyReportController.php
@@ -52,57 +52,84 @@ class MonthlyReportController extends Controller
     }
 
     public function exportExcel()
-{
-    
-    // Inisialisasi Spreadsheet
-    $spreadsheet = new Spreadsheet();
+    {
 
-    $parts = $this->_partRepository->getAll();
-    $partcategories = $this->_partCategoryRepository->getAll();
-    $partcategories = $this->_partCategoryRepository->getAll();
+        // Inisialisasi Spreadsheet
+        $spreadsheet = new Spreadsheet();
 
-    // Buat objek worksheet
-    $sheet = $spreadsheet->getActiveSheet();
-    $sheet->mergeCells('A12:A14');
-    $sheet->mergeCells('B12:B14');
-    $sheet->mergeCells('C12:E13');
-    $sheet->getColumnDimension('A')->setWidth(30); // Mengatur lebar kolom A menjadi 15
-    $sheet->getColumnDimension('B')->setWidth(30); // Mengatur lebar kolom A menjadi 15
-    $sheet->getColumnDimension('C')->setWidth(15); // Mengatur lebar kolom A menjadi 15
-    $sheet->getColumnDimension('D')->setWidth(15); // Mengatur lebar kolom A menjadi 15
-    $sheet->getColumnDimension('E')->setWidth(15); // Mengatur lebar kolom A menjadi 15
-    $sheet->getStyle('A1:AZ100')->getAlignment()->setHorizontal('center'); // Mengatur teks rata tengah
-    $sheet->getStyle('A1:AZ100')->getAlignment()->setVertical('center'); // Mengatur teks rata tengah
+        $parts = $this->_partRepository->getAll();
+        $partcategories = $this->_partCategoryRepository->getAll();
+        $partcategories = $this->_partCategoryRepository->getAll();
 
-    // Set data ke sel-sel
-    if (sizeof($partcategories) > 0) {
-        $row = 15; // Initialize the row variable
-        foreach ($partcategories as $partCategory) {
-            $cell = 'A' . $row; // Build the cell reference with the current row
-            $sheet->setCellValue($cell, $partCategory->part_category_name);
-            $row++; // Increment the row variable for the next iteration
+        // Buat objek worksheet
+        $sheet = $spreadsheet->getActiveSheet();
+
+        //Merge
+        $sheet->mergeCells('A12:A14');
+        $sheet->mergeCells('B12:B14');
+        $sheet->mergeCells('C12:E13');
+
+        //Dimensions
+        $sheet->getColumnDimension('A')->setWidth(30); // Mengatur lebar kolom A menjadi 15
+        $sheet->getColumnDimension('B')->setWidth(30); // Mengatur lebar kolom A menjadi 15
+        $sheet->getColumnDimension('C')->setWidth(15); // Mengatur lebar kolom A menjadi 15
+        $sheet->getColumnDimension('D')->setWidth(15); // Mengatur lebar kolom A menjadi 15
+        $sheet->getColumnDimension('E')->setWidth(15); // Mengatur lebar kolom A menjadi 15
+
+        //Style
+        $sheet->getStyle('A1:AZ100')->getAlignment()->setHorizontal('center'); // Mengatur teks rata tengah
+        $sheet->getStyle('A1:AZ100')->getAlignment()->setVertical('center'); // Mengatur teks rata tengah
+
+        //Single Cells/Manual Labor
+        $sheet->setCellValue('A12', 'Inventory');
+        $sheet->setCellValue('B12', 'Import/Lokal');
+        $sheet->setCellValue('C12', 'Begin August-23');
+        $sheet->setCellValue('C14', 'QTY');
+        $sheet->setCellValue('D14', 'AMOUNT(USD)');
+        $sheet->setCellValue('E14', 'AMOUNT(IDR)');
+        $sheet->setCellValue('F14', 'IN GIT AUGUST');
+
+        //Dynamics/Loops
+        if (sizeof($partcategories) > 0) {
+            $row = 15; // Initialize the row variable
+            foreach ($partcategories as $partCategory) {
+                $cellStart = 'A' . $row; // Build the starting cell reference with the current row
+                $cellEnd = 'A' . ($row + 1); // Build the ending cell reference in the next row
+
+                // Merge the two cells vertically (below)
+                $sheet->mergeCells($cellStart . ':' . $cellEnd);
+
+                // Set the value in the merged cell
+                $sheet->setCellValue($cellStart, $partCategory->part_category_name);
+
+                $row += 2; // Increment the row variable by 2 for the next iteration (to skip the merged cell)
+            }
         }
+
+        $sheet->setCellValue("B15", "Import");
+
+        for ($i = 15; $i < 15 + (sizeof($partcategories) * 2); $i++) {
+            $cellStart = 'B' . $i;
+
+            if ($i % 2 == 0) {
+                $sheet->setCellValue($cellStart, "Lokal");
+            } else {
+                $sheet->setCellValue($cellStart, "Import");
+            }
+
+        }
+
+        $currentMonth = date('F');
+        // Buat file Excel
+        $writer = new Xlsx($spreadsheet);
+        $filename = "Monthly Report - $currentMonth.xlsx";
+
+        // Simpan file Excel
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+        $writer->save('php://output');
     }
-
-    $sheet->setCellValue('A12', 'Inventory');
-    $sheet->setCellValue('B12', 'Import/Lokal');
-    $sheet->setCellValue('C12', 'Begin August-23');
-    $sheet->setCellValue('C14', 'QTY');
-    $sheet->setCellValue('D14', 'AMOUNT(USD)');
-    $sheet->setCellValue('E14', 'AMOUNT(IDR)');
-    $sheet->setCellValue('F14', 'IN GIT AUGUST');
-
-
-    // Buat file Excel
-    $writer = new Xlsx($spreadsheet);
-    $filename = 'merged_cells_example.xlsx';
-
-    // Simpan file Excel
-    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    header('Content-Disposition: attachment;filename="' . $filename . '"');
-    header('Cache-Control: max-age=0');
-    $writer->save('php://output');
-}
 
     /**
      * Store a newly created resource in storage.
