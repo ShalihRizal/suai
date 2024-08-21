e11aa7b5bc5a7e13c458bd108f7e347161d33142
ShalihRizal <rizalpangandaran9@gmail.com>
2024-08-21 11:25:34 +0700

    fix lopr report

--- app/Exports/listofpartreqexport.php
+++ app/Exports/listofpartreqexport.php
@@ -28,14 +28,62 @@ class listofpartreqexport implements FromCollection, ShouldAutoSize
         // Fetch data from the PartRepository
         $data = $this->partRepository->getAll();
 
-        // Format the data as required for export
-        $formattedData = [['Name', 'Part Number']];
+        // Define the header row with all the specified field names
+        $formattedData = [
+            [
+                'Part Req Number',
+                'Part No',
+                'Part Name',
+                'Part Qty',
+                'Carline',
+                'Carname',
+                'Alasan',
+                'Order',
+                'Shift',
+                'Machine No',
+                'Applicator No',
+                'Wear And Tear Code',
+                'Serial No',
+                'Side No',
+                'Stroke',
+                'Pic',
+                'Remarks',
+                'Status',
+                'Approved By',
+                'Wear And Tear Status',
+                'Tanggal Dibuat',
 
+            ]
+        ];
 
+        // Loop through the data and add each row to the formatted data
         foreach ($data as $item) {
-            $formattedData[] = [$item->part_name, $item->part_no]; // Adjust the columns as per your needs
+            $formattedData[] = [
+                $item->part_req_number,
+                $item->part_no,
+                $item->part_name,
+                $item->part_qty,
+                $item->carline_name,
+                $item->carname_name,
+                $item->alasan,
+                $item->order,
+                $item->shift,
+                $item->machine_no,
+                $item->applicator_no,
+                $item->wear_and_tear_code,
+                $item->serial_no,
+                $item->side_no,
+                $item->stroke,
+                $item->pic,
+                $item->remarks,
+                $item->status,
+                $item->user_name,
+                $item->wear_and_tear_status,
+                $item->created_at,
+            ];
         }
 
         return collect($formattedData);
     }
+
 }
